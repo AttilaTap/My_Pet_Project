666e4caffd993a538e904022de65a12301e50ab5
Attila_Tapai <attila.tapai01@gmail.com>
2023-07-14 09:46:40 +0200

    styled comp button

--- pages/components/button.js
+++ pages/components/button.js
@@ -1,18 +1,46 @@
 import { useSession, signIn, signOut } from 'next-auth/react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: #4caf50;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border: none;
+  border-radius: 4px;
+  transition-duration: 0.4s;
+
+  &:hover {
+    background-color: #45a049;
+  }
+`;
+
+const Text = styled.p`
+  color: #f2f2f2;
+  font-size: 18px;
+`;
+
 export default function Component() {
   const { data: session } = useSession();
+  
   if (session) {
     return (
       <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
+        <Text>Signed in as {session.user.email}</Text>
+        <Button onClick={() => signOut()}>Sign out</Button>
       </>
     );
   }
   return (
     <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <Text>Not signed in</Text>
+      <Button onClick={() => signIn()}>Sign in</Button>
     </>
   );
 }
+
